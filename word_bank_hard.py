d5916ce3dd88e0fefc16ebce3c5dd3b991ce8601
Yahia Nassab <ynassab@edu.uwaterloo.ca>
2020-12-30 23:46:33 -0500

    Some word bank additions

--- word_bank_hard.py
+++ word_bank_hard.py
@@ -2,128 +2,125 @@
 # Hard difficulty
 
 bank = {
-    
-    # Animals
-    "GOAT": "Animal whose coat produces angolla and cashmere sweaters",
-    "GIZZARD": "Second stomach found in all birds",
-    "BUMBLEBEE BAT": "Smallest mammal",
-    "GIRAFFE": "Animal with the highest blood pressure",
-    
-    
-    # Books
-    "JOHN GREEN": "Wrote \"Turtles all the Way Down\"",
-    "BRAM STOKER": "Wrote \"Dracula\"",
-    "THE RETURN OF THE KING": "Last volume in \"The Lord of the Rings\"",
-    "SIR ARTHUR CONAN DOYLE": "Wrote \"Sherlock Holmes\"",
-    "KURT VONNEGUT": "Wrote \"Slaughterhouse-Five\"",
-    "STEPHEN KING": "Wrote \"The Stand\"",
-    "WASHINGTON IRVING": "Wrote \"The Legend of Sleepy Hollow\"",
-    "ICHABOD CRANE": "Protagonist in \"The Legend of Sleepy Hollow\"",    
-    
-    
-    # Film
-    "LOVE ME TENDER": "First movie Elvis Presley appeared in",
-    "CATCH ME IF YOU CAN": "Leonardo DiCaprio plays as Frank Abignale",
-    "SPIDERMAN": "Toby Maguire, Andrew Garfield, Tom Holland",
-    "UNSTOPPABLE": "Denzel Washington and Chris Pine try to stop a train",
-    "SPIRITED AWAY": "10 year-old Chihiro finds herself in the spirit world",
-    
-    # Television
-    "ASHLEY TISDALE": "Played Maddie Fitzpatrick in \"The Suite Life of Zack and Cody\"",
-    "THE BRADY BUNCH": "Robert Reed as Mike Brady in this late 60's sitcom",
-    "THE COSBY SHOW": "Sitcom about a Brooklyn family, the Huxtables (1984-1992)",
-    
-    # Food
-    "TAHINI": "Sesame seed paste originating in the Middle East",
-    "SUSHI": "Japanese for \"vinegar rice\"",
-    "SCOVILLE": "Spiciness scale of peppers",
-    "PAPRIKA": "Spice commonly found in Hungarian cuisine alongside salt",
-    "LACTOSE": "Milk disaccharide",
-    "STURGEON": "Family of fish whose roe produces caviar",
-    "MONOSODIUM GLUTAMATE": "MSG stands for:",
-    "SUCCOTASH": "North American Indian dish made from corns and beans",
-    
-
-    # Geography
-    "TIGRIS AND EUPHRATES": "The Fertile Crescent rivers",
-    "TUNISIA": "Real-life Tatouine, or Tataouine, in this country",
-    "ACONCAGUA": "Largest mountain in South America",
-    "GIBRALTAR STRAIT": "Body of water between Spain and Morocco",
-    "ISTANBUL": "Only city in two continents",
-    "OLYMPUS MONS": "Largest volcano on Mars",
-    "URAL MOUNTAINS": "Boundary between Europe and Asia",
-    "AMAZON": "World's fastest river, in terms of water volume discharge",
-    "TURKEY": "Anatolia, Bursa, Izmir",
-    "PORTUGAL": "Porto, Coimbra, Braga",
+	
+	# Animals
+	"GOAT": "Animal whose coat produces angolla and cashmere sweaters",
+	"GIZZARD": "Second stomach found in all birds",
+	"BUMBLEBEE BAT": "Smallest mammal",
+	"GIRAFFE": "Animal with the highest blood pressure",
+	
+	# Books
+	"JOHN GREEN": "Wrote \"Turtles all the Way Down\"",
+	"BRAM STOKER": "Wrote \"Dracula\"",
+	"THE RETURN OF THE KING": "Last volume in \"The Lord of the Rings\"",
+	"SIR ARTHUR CONAN DOYLE": "Wrote \"Sherlock Holmes\"",
+	"KURT VONNEGUT": "Wrote \"Slaughterhouse-Five\"",
+	"STEPHEN KING": "Wrote \"The Stand\"",
+	"WASHINGTON IRVING": "Wrote \"The Legend of Sleepy Hollow\"",
+	"ICHABOD CRANE": "Protagonist in \"The Legend of Sleepy Hollow\"",
+	
+	# Film
+	"LOVE ME TENDER": "First movie Elvis Presley appeared in",
+	"CATCH ME IF YOU CAN": "Leonardo DiCaprio plays as Frank Abignale",
+	"SPIDERMAN": "Toby Maguire, Andrew Garfield, Tom Holland",
+	"UNSTOPPABLE": "Denzel Washington and Chris Pine try to stop a train",
+	"SPIRITED AWAY": "10 year-old Chihiro finds herself in the spirit world",
+	
+	# Television
+	"ASHLEY TISDALE": "Played Maddie Fitzpatrick in \"The Suite Life of Zack and Cody\"",
+	"THE BRADY BUNCH": "Robert Reed as Mike Brady in this late 60's sitcom",
+	"THE COSBY SHOW": "Sitcom about a Brooklyn family, the Huxtables (1984-1992)",
+	
+	# Food
+	"BURRITO": "Food whose name comes from the word for \"donkey\" in Spanish",
+	"LACTOSE": "Milk disaccharide",
+	"STURGEON": "Family of fish whose roe produces caviar",
+	"MONOSODIUM GLUTAMATE": "MSG stands for:",
+	"SUCCOTASH": "North American Indian dish made from corns and beans",
+	
+	# Geography
+	"TIGRIS AND EUPHRATES": "The Fertile Crescent rivers",
+	"TUNISIA": "Real-life Tatouine, or Tataouine, in this country",
+	"ACONCAGUA": "Largest mountain in South America",
+	"STRAIT OF GIBRALTAR": "Body of water between Spain and Morocco",
+	"ISTANBUL": "Only city in two continents",
+	"OLYMPUS MONS": "Largest volcano on Mars",
+	"URAL MOUNTAINS": "Boundary between Europe and Asia",
+	"AMAZON": "World's fastest river, in terms of water volume discharge",
+	"TURKEY": "Anatolia, Bursa, Izmir",
+	"PORTUGAL": "Porto, Coimbra, Braga",
  
-    # Geography, Capitals
-    "CANBERRA": "Capital of Australia",
-    "LISBON": "Capital of Portugal",
-    "RIYADH": "Capital of Saudi Arabia",
-    "TALLAHASSEE": "Capital of Florida, United States",
-    "TRENTON": "Capital of New Jersey, United States",
-    
-    # History
-    "AGINCOURT": "Major English victory in the Hundred Years War - 25 October 1415",
-    "HASTINGS": "Battle that began a Norman conquest of England - 14 October 1066",
-    "HADRIAN": "Roman Emperor that ordered the building of a wall to keep Picts and Scots out of England",
-    "MACBETH": "King of Scotland between 1040 and 1057",
-    "FIDEL CASTRO": "Led an attack on the Moncada Army Barracks on July 26 1953",
-    
-    # Linguistics
-    "PER SE": "Latin for \"in or by itself\"",
-    "POST MORTEM": "Latin for \"after death\"",
-    "IN SITU": "Latin for \"in its original place\"",
-    "ET CETERA": "Latin for \"and the rest\" (often abbreviated)",
-    "A PRIORI": "Latin for \"reasoning from causes to effects\"",
-    "AMPERSAND": "Name of this character: &",
-    "OCTOTHORPE": "Name of this character: #",
-    "CAROT": "Name of this character: ^",
+	# Geography, Capitals
+	"CANBERRA": "Capital of Australia",
+	"ULAANBAATAR": "Capital of Mongolia",
+	"RIYADH": "Capital of Saudi Arabia",
+	"TALLAHASSEE": "Capital of Florida, United States",
+	"TRENTON": "Capital of New Jersey, United States",
+	
+	# History
+	"AGINCOURT": "Major English victory in the Hundred Years War - 25 October 1415",
+	"HASTINGS": "Battle that began a Norman conquest of England - 14 October 1066",
+	"HADRIAN": "Roman Emperor that ordered the building of a wall to keep Picts and Scots out of England",
+	"MACBETH": "King of Scotland between 1040 and 1057",
+	"FIDEL CASTRO": "Led an attack on the Moncada Army Barracks on July 26 1953",
+	
+	# Linguistics
+	"PER SE": "Latin for \"in or by itself\"",
+	"POST MORTEM": "Latin for \"after death\"",
+	"IN SITU": "Latin for \"in its original place\"",
+	"ET CETERA": "Latin for \"and the rest\" (often abbreviated)",
+	"A PRIORI": "Latin for \"reasoning from causes to effects\"",
+	"AMPERSAND": "Name of this character: &",
+	"OCTOTHORPE": "Name of this character: #",
+	"CAROT": "Name of this character: ^",
+	"HIRAGANA": "Simplified, phonetic writing system for Japanese",
+	
+	# Music
+	"TIME OF OUR LIVES": "Pitbull + Ne-Yo in 2014",
+	"FLEETWOOD MAC": "Albums \"Rumours\" in 1977 and \"Say You Will\" in 2003",
+	"BILLY RAY CYRUS": "Released the song \"Achy Breaky Heart\" in 1992",
+	"TRAIN": "Lead singer: Patrick Monahan",
+	"FALL OUT BOY": "Lead singer: Patrick Stump",
+	"ONEREPUBLIC": "Lead singer: Ryan Tedder (since 2002)",	
+	
+	# People
+	"JENNIFER LAWRENCE": "Highest paid actress in the world - 2015 and 2016",
+	"INDIRA GANDHI": "Prime minister of India after Lal Bahadur Shastri",
+	"AL GORE": "Elected Senator for Tennessee in 1985, later to run for president",
+	"JOHANNES GUTENBERG": "Invented the Printing Press in the mid 15th century",
+	
+	# Phobias
+	"SPEED": "Tachophobia is the fear of what?",
+	"PALINDROMES": "Aibophobia is the fear of what?",
+	"BIRDS": "Ornithophobia is the fear of what?",
+	
+	# Biology
+	"THYROID": "Endocrine gland located in the neck, in front of the trachea",
+	"DUODENUM": "First and shortest segment of the small intestine",
+	
+	# Chemistry
+	"POLYETHYLENE": "Most common polymer structure, usually categorized as either high or low density",
+	"DIASTEREOMERS": "Stereoisomers of a compound that are not mirror images of each other",
+	
+	# Physics
+	"ACTION": "Physical quantity whose SI units are joule seconds",
+	"HAMILTONIAN": "Energy operator in Quantum Mechanics",
+	"FERMIONS": "The Pauli Exclusion Principle applies to these types of particles",
+	
+	# Astronomy
+	"PARSEC": "Distance at which the mean radius of Earth's orbit subtends one arcsecond",
+	"BETELGEUSE": "Red giant which is the shoulder of Orion's upwards-reaching arm",
+	"VENUS": "Heavenly body with an apparent magnitude between -4.92 and -2.98",
+	"CHARON": "Pluto's largest moon",
+	
+	# Sports
+	"NEW YORK KNICKS": "Won the first ever NBA game",
+	"BALTIMORE ORIOLES": "Babe Ruth played on this team before joining the Boston Red Sox",
+	"CAL RIPKEN JR.": "Played the most consecutive games of baseball",
+	"DON SHULA": "Has the most wins as a coach in the NFL",
+	"ANFIELD": "Home stadium of Liverpool Football Club",
+	"ITALY": "Won the 2006 FIFA World Cup",
 
-    
-    # Music
-    "TIME OF OUR LIVES": "Pitbull + Ne-Yo in 2014",
-    "HAPPIER": "Marshmello + Bastille in 2018",
-    "FLEETWOOD MAC": "Albums \"Rumours\" in 1977 and \"Say You Will\" in 2003",
-    "BILLY RAY CYRUS": "Released the song \"Achy Breaky Heart\" in 1992",
-    "TRAIN": "Lead singer: Patrick Monahan",
-    "FALL OUT BOY": "Lead singer: Patrick Stump",
-    "ONEREPUBLIC": "Lead singer: Ryan Tedder (since 2002)",    
-    
-    
-    # People
-    "JENNIFER LAWRENCE": "Highest paid actress in the world - 2015 and 2016",
-    "INDIRA GANDHI": "Prime minister of India after Lal Bahadur Shastri",
-    "AL GORE": "Elected Senator for Tennessee in 1985, later to run for president",
-    
-    #Phobias
-    "SUBWAY TRAINS": "Bathysiderodromophobia is the fear of what?",
-    "PALINDROMES": "Aibophobia is the fear of what?",
-    
-    # Science
-    "THYROID": "Endocrine gland located in the neck, in front of the trachea",
-    "DUODENUM": "First and shortest segment of the small intestine",
-    "HAMILTONIAN": "Energy operator in Quantum Mechanics",
-    "DIASTEREOMERS": "Stereoisomers of a compound that are not mirror images of each other",
-    "FERMIONS": "The Pauli Exclusion Principle applies to these types of particles",
-    
-    # Astronomy
-    "PARSEC": "Distance at which the mean radius of Earth's orbit subtends one arcsecond",
-    "BETELGEUSE": "Red giant which is the shoulder of Orion's upwards-reaching arm",
-    
-    # Sports
-    "NEW YORK KNICKS": "Won the first ever NBA game",
-    "BALTIMORE ORIOLES": "Babe Ruth played on this team before joining the Boston Red Sox",
-    "CAL RIPKEN JR.": "Played the most consecutive games of baseball",
-    "DON SHULA": "Has the most wins as a coach in the NFL",
-    "ANFIELD": "Home stadium of Liverpool Football Club",
-    
-    
-    
-    # Theatre
-    
-    
-    # Other
+	# Other
 
-    
-            }
+			}
--- word_bank_normal.py
+++ word_bank_normal.py
@@ -2,113 +2,126 @@
 # Normal difficulty
 
 bank = {
-    
-    # Animals
-    "HUMMINGBIRD": "Only bird capable of flying backwards",
-    "BOVINE": "Cats are to \"feline\" as cows are to ______",
-    "MURDER": "A group of crows is called a ______",
-    
-    # Books
-    "J.K. ROWLING": "Author of the \"Harry Potter\" series",
-    "ROALD DAHL": "Wrote \"Charlie and the Chocolate Factory\"",
-    "KATNISS EVERDEEN": "Protagonist in the Hunger Games",
-    "KRYPTON": "Superman's birth planet",
-    "WORLD WAR Z": "Maz Brooks novel that is an oral history of a zombie war",
-    
-    # Film
-    "BRAIN": "What the scarecrow wanted in the Wizard of Oz",
-    "MILLENIUM FALCON": "Han Solo's ship",
-    "TAKEN": "Liam Neeson pursues his daughter's kidnappers",
-    "KUNG FU PANDA": "Jack Black voices Po in this Dreamworks film",
-    "RATATOUILLE": "Remy is rat, but a great artist can come from anywhere, n'est pas?",
-    "DESPICABLE ME": "Gru trying to be a supervillain",
-    "LOGAN": "Hugh Jackman as a superhero",
-    "INCEPTION": "Leonardo DiCaprio is in a dream inside a dream inside a dream...",
-    
-    
-    # Food
-    "BURRITO": "Food whose name comes from the word for \"donkey\" in Spanish",
-    "RAW CHICKEN": "Leading cause of salmonella poisoning",
-    "CAMPBELL'S": "Condensed soup co. established in 1869",
-    "MUSHROOM": "Cremini, oyster, morel, wood ear",
-    "APPLE": "Isaac Newton was inspired by this fruit falling on his head",
+	
+	# Animals
+	"HUMMINGBIRD": "Only bird capable of flying backwards",
+	"BOVINE": "Cats are to \"feline\" as cows are to ______",
+	"MURDER": "A group of crows is called a ______",
+	
+	# Books
+	"J.K. ROWLING": "Author of the \"Harry Potter\" series",
+	"ROALD DAHL": "Wrote \"Charlie and the Chocolate Factory\"",
+	"KATNISS EVERDEEN": "Protagonist in the Hunger Games",
+	"KRYPTON": "Superman's birth planet",
+	"WORLD WAR Z": "Maz Brooks novel that is an oral history of a zombie war",
+	
+	# Film
+	"BRAIN": "What the scarecrow wanted in the Wizard of Oz",
+	"MILLENIUM FALCON": "Han Solo's ship",
+	"TAKEN": "Liam Neeson pursues his daughter's kidnappers",
+	"KUNG FU PANDA": "Jack Black voices Po in this Dreamworks film",
+	"RATATOUILLE": "Remy is a rat, but a great artist can come from anywhere, n'est pas?",
+	"DESPICABLE ME": "Gru trying to be a supervillain, unsuccessfully",
+	"LOGAN": "Hugh Jackman as a superhero",
+	"INCEPTION": "Leonardo DiCaprio is in a dream inside a dream inside a dream...",
+	
+	# Food
+	"TAHINI": "Sesame seed paste originating in the Middle East",
+	"SUSHI": "Japanese for \"vinegar rice\"",
+	"SCOVILLE": "Spiciness scale of peppers",
+	"PAPRIKA": "Spice commonly found in Hungarian cuisine alongside salt",
+	"RAW CHICKEN": "Leading cause of salmonella poisoning",
+	"CAMPBELL'S": "Condensed soup co. established in 1869",
+	"MUSHROOM": "Cremini, oyster, morel, wood ear",
+	"APPLE": "Isaac Newton was inspired by this fruit falling on his head",
 
-    # Geography
-    "ANTARCTICA": "Largest continent",
-    "CHILE": "More than half of the West South American coastline",
-    "PERU": "Machu Picchu country",
-    "DAMASCUS": "Oldest city in the world",
-    "CANADA": "Longest coastline",
-    "SEINE": "River in Paris",
-    "VATICAN CITY": "Smallest independent country on Earth",
-    "MARIANA TRENCH": "Deepest place on Earth",
-    "BOLIVIA": "Country named after Simon Bolivar",
-    "RING OF FIRE": "Region where many most of the world's volcanic eruptions occur",
-    "GERMANY": "Munich, Cologne, Frankfurt",
-    "ENGLAND": "Manchester, Liverpool, Birmingham",
-    "SCOTLAND": "Edinburgh, Glasgow, Dundee",
-    "IRELAND": "Dublin, Cork, Galway",
+	# Geography
+	"ANTARCTICA": "Largest continent",
+	"CHILE": "More than half of the West South American coastline",
+	"PERU": "Machu Picchu country",
+	"DAMASCUS": "Oldest city in the world",
+	"CANADA": "Longest coastline",
+	"SEINE": "River in Paris",
+	"VATICAN CITY": "Smallest independent country on Earth",
+	"MARIANA TRENCH": "Deepest place on Earth",
+	"BOLIVIA": "Country named after Simon Bolivar",
+	"RING OF FIRE": "Region where many most of the world's volcanic eruptions occur",
+	"GERMANY": "Munich, Cologne, Frankfurt",
+	"ENGLAND": "Manchester, Liverpool, Birmingham",
+	"SCOTLAND": "Edinburgh, Glasgow, Dundee",
+	"IRELAND": "Dublin, Cork, Galway",
  
-    # Geography, Capitals
-    "OTTAWA": "Capital of Canada",
-    "BEIJING": "Capital of China",
-    "MOSCOW": "Capital of Russia",
-    "CAIRO": "Capital of Egypt",
-    "AUSTIN": "Capital of Texas, United States",
-    
-    # History
-    "THE GREAT PYRAMIDS OF GIZA": "Wonder built by the ancient Egypts",
-    "PEARL HARBOR": "The bombing of this military base lead the U.S. to join WWII",
-    "BERLIN WALL": "Erected between East and West Germany in 1961",
-    
-    # Linguistics
-    "ASTERISK": "Name for this character: *",
-    "GERMANIC": "Language family that English falls under",
-    "ROMANTIC": "Language family that Italian falls under",
-    "FARSI": "Another name for the Persian language",
-    
-    # Music
-    "TAYLOR SWIFT": "Released the song \"I Knew You Were Trouble\" in 2012",
-    "MAROON 5": "Released the song \"Moves Like Jagger\" in 2010",
-    "LINKIN PARK": "Released the album \"Hybrid Theory\" in 2000",
-    "PANIC! AT THE DISCO": "Band whose sole member is Brendon Urie",
-    "VANCE JOY": "\"Riptide\", \"Georgia\", \"Mess is Mine\"",
-    "PARAMORE": "\"Ain't it Fun\", \"Still into You\", \"Misery Business\"",
-    
-    
-    # People
-    "ALBERT EINSTEIN": "Physicist famous for his Theory of Special Relativity",
-    "NAPOLEON BONAPARTE": "Leader in the French Revolution",
-    "JULIUS CAESAR": "Roman politician and military general, stabbed 23 times",
-    "BENEDICT CUMBERBATCH": "Plays Sherlock Holmes in BBC's \"Sherlock\"",
-    "VIN DIESEL": "Real voice of \"I am Groot\"",
-    "ELLEN DEGENERES": "Voices Dory in \"Finding Nemo\" and \"Finding Dory\"",
-    "ELON MUSK": "Tesla, SpaceX, PayPal",
-    "WARREN BUFFETT": "CEO of Berkshire Hathaway",
-    
-    
-    # Science
-    "BALL AND SOCKET": "The shoulder is this type of bone joint",
-    "STOMACH": "The prefix \"gastro-\" refers to this part of the body",
-    "OPTICAL NERVE": "This connecting to the retina leads to a blind spot in the eye",
-    "TABLE SALT": "The common name for sodium chloride",
-    "BAKING SODA": "The common name for sodium bicarbonate",
-    "HYDROGEN": "The lightest element",
-    "PHOTON": "A \"particle\" or \"packet\" of light",
-    "NEWTON": "SI unit for force",
-    
-    # Astronomy
-    "JUPITER": "Fifth planet from the Sun",
-    "BIG DIPPER": "Formed from the seven brighest stars in the Ursa Major constellation",
-    
-    # Sports
-    "LACROSSE": "Official Summer sport of Canada",
-    "LEBRON JAMES": "Played for the Cavaliers, Heat, back to the Cavaliers, and now the Lakers as of 2018",
-    "ITALY": "Won the 2006 FIFA World Cup",
-    
-    
-    # Other
-    
-    
+	# Geography, Capitals
+	"OTTAWA": "Capital of Canada",
+	"BEIJING": "Capital of China",
+	"MOSCOW": "Capital of Russia",
+	"CAIRO": "Capital of Egypt",
+	"LISBON": "Capital of Portugal",
+	"AUSTIN": "Capital of Texas, United States",
+	
+	# History
+	"THE GREAT PYRAMIDS OF GIZA": "Wonder built by the ancient Egypts",
+	"PEARL HARBOR": "The bombing of this military base lead the U.S. to join WWII",
+	"BERLIN WALL": "Erected between East and West Germany in 1961",
+	"INDIA": "Gained independence from England on August 15, 1947"
+	
+	# Linguistics
+	"ASTERISK": "Name for this character: *",
+	"TILDE": "Name for this character: ~",
+	"GERMANIC": "Language family that English falls under",
+	"ROMANTIC": "Language family that Italian falls under",
+	"FARSI": "Another name for the Persian language",
+	"MANDARIN": "Most commonly spoken language in China",
+		
+	# Music
+	"TAYLOR SWIFT": "Released the song \"I Knew You Were Trouble\" in 2012",
+	"MAROON 5": "Released the song \"Moves Like Jagger\" in 2010",
+	"LINKIN PARK": "Released the album \"Hybrid Theory\" in 2000",
+	"PANIC! AT THE DISCO": "Band whose sole member is Brendon Urie",
+	"VANCE JOY": "\"Riptide\", \"Georgia\", \"Mess is Mine\"",
+	"PARAMORE": "\"Ain't it Fun\", \"Still into You\", \"Misery Business\"",
+	"HAPPIER": "Marshmello + Bastille in 2018",
+	
+	# People
+	"ALBERT EINSTEIN": "Physicist famous for his Theory of Special Relativity",
+	"NAPOLEON BONAPARTE": "Leader in the French Revolution",
+	"JULIUS CAESAR": "Roman politician and military general, stabbed 23 times",
+	"BENEDICT CUMBERBATCH": "Plays Sherlock Holmes in BBC's \"Sherlock\"",
+	"VIN DIESEL": "Real voice of \"I am Groot\"",
+	"ELLEN DEGENERES": "Plays the voice of Dory in \"Finding Nemo\" and \"Finding Dory\"",
+	"ELON MUSK": "Tesla, SpaceX, PayPal",
+	"WARREN BUFFETT": "CEO of Berkshire Hathaway",
+	
+	# Phobias
+	"HEIGHTS": "Acrophobia is the fear of what?",
+	"BLOOD": "Hemophobia is the fear of what?",
+	"ANIMALS": "Zoophobia is the fear of what?",
+	
+	# Biology
+	"BALL AND SOCKET": "The shoulder is this type of bone joint",
+	"STOMACH": "The prefix \"gastro-\" refers to this part of the body",
+	"OPTICAL NERVE": "This connecting to the retina leads to a blind spot in the eye",
+	
+	# Chemistry
+	"TABLE SALT": "The common name for sodium chloride",
+	"BAKING SODA": "The common name for sodium bicarbonate",
+	"HYDROGEN": "The lightest element in the Periodic Table",
+	
+	# Physics
+	"PHOTON": "A \"particle\" or \"packet\" of light",
+	"NEWTON": "SI unit for force",
+	
+	# Astronomy
+	"JUPITER": "Fifth planet from the Sun",
+	"BIG DIPPER": "Formed from the seven brighest stars in the Ursa Major constellation (North American name)",
+	"POLARIS": "More commonly known as the North Star",
+	
+	# Sports
+	"LACROSSE": "Official summer sport of Canada",
+	"LEBRON JAMES": "Played for the Cavaliers, Heat, back to the Cavaliers, and then the Lakers in 2018",
+	"RUSSIA": "Hosted the 2018 FIFA World Cup",
+	"SOUTH KOREA": "Hosted the 2018 Winter Olympic Games"
+	
+	# Other
 
-            }
+			}

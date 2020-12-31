1372d15251f177c5cf40b8a06cb2c424bb1c2931
Yahia Nassab <ynassab@edu.uwaterloo.ca>
2020-12-31 00:31:55 -0500

    Minor formatting changes

--- word_bank_drunk.py
+++ word_bank_drunk.py
@@ -2,80 +2,59 @@
 # Drunk "difficulty"
 
 bank = {
-    
-    
-    # Better names
-    "TOASTER": "Tanning bed for bread",
-    "GLOVES": "Finger pants",
-    "TISSUES": "Sneeze paper",
-    "AIR HORN": "Spray-SCREAM",
-    "CABLE": "Electric hose",
-    "TORTILLAS": "Burrito blankets",
-    "CAMEL": "Sand moose",
-    "KILLER WHALE": "Panda fish",
-    "BENCH": "People shelf",
-    "BATH TUB": "Reverse boat",
-    "MUFFIN": "Bread mushroom",
-    "PORCUPINE": "Stab rabbit",
-    "WAFFLES": "Pancakes with abs",
-    "ONION RINGS": "Vegetable donuts",
-    "LOBSTERS": "Mermaid scorpions",
-    "LASAGNA": "Spaghetti cake",
-    "MINTY": "Cold spicy",
-    "CARPET": "Hair for your floor",
-    "APPLE JUICE": "iPhone chargers",
-    "CLAPPING": "High-fiving yourself",
-    "CANADA GEESE": "Canadian cobra chickens",
-    "CUTLERY": "Food weapons",
-    "GIRAFFES": "leopard-moose-camels with six-foot necks",
-    
-    
-    # Puns
-    "FUNGI": "A mushroom is a _____ to have at parties",
-    "POWER NAP": "Exercising your authority and sleeping on someone weaker than you",
-    "BROKER": "Person who makes you go broke",
-    "CATHOLIC": "Photons have mass. Therefore they are:",
-    "SOY MILK": "Milk introducing itself in Spanish",
-    "MOSES": "First person to use Ctrl-C",
-    "ODD NUMBERS": "Literally can't even.",
-    "TWITTER": "Voldemort has followers, not friends. Therefore, he uses:",
-    
-    
-    # Shower thoughts
-    
-    "JAPAN": "Has a flag that is a pie chart of how much of itself is itself",
-    "CANADA GEESE": "Proof that dinosaurs would still be scary with feathers",
-    "ANTARCTICA": "Island that only has a northern coast",
-    "BAYONET": "Bringing a knife to a gun fight",
-    "VAMPIRES": "If telescopes work using mirrors, then you'll never know if these exist in space",
-    "STOMACH": "Thinks all potatoes are mashed",
-    "HORSES": "Vehicles with automatic collision avoidance and the capability of self-driving home when you're asleep",
-        
-    
-    # Book synopses
-    "THE ILLIAD": "Book where Achilles spends ten years punching people in the face",
-    
 
-    
-    # Quotes
-    "GOOGLE STREET VIEW": "\"Yeah, just send someone to drive down every single road on the planet\"",
-    "LUIS SUAREZ": "\"But ref! I only bit someone like 3 times. If you consider all the games I've played, that's almost a perfect record!\"",
-    "LEEEROOOOOOOOY JENKINNNSSSS": "\"Alright chums, I'm back. Let's do this. *inhales*\"",
-    "CELSIUS": "\"On a scale from 0 to 100, how hot is your water?\"",
-    "THE LOS ANGELES ANGELS": "\"The The Angels Angels\""
-
-    
-    
-    
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-#     "": "",
-
-            }
+	# Better names
+	"TOASTER": "Tanning bed for bread",
+	"GLOVES": "Finger pants",
+	"TISSUES": "Sneeze paper",
+	"AIR HORN": "Spray-SCREAM",
+	"CABLE": "Electric hose",
+	"TORTILLAS": "Burrito blankets",
+	"CAMEL": "Sand moose",
+	"KILLER WHALE": "Panda fish",
+	"BENCH": "People shelf",
+	"BATH TUB": "Reverse boat",
+	"MUFFIN": "Bread mushroom",
+	"PORCUPINE": "Stab rabbit",
+	"WAFFLES": "Pancakes with abs",
+	"ONION RINGS": "Vegetable donuts",
+	"LOBSTERS": "Mermaid scorpions",
+	"LASAGNA": "Spaghetti cake",
+	"MINTY": "Cold spicy",
+	"CARPET": "Hair for your floor",
+	"APPLE JUICE": "iPhone chargers",
+	"CLAPPING": "High-fiving yourself",
+	"CANADA GEESE": "Canadian cobra chickens",
+	"CUTLERY": "Food weapons",
+	"GIRAFFES": "leopard-moose-camels with six-foot necks",
+	
+	# Puns
+	"FUNGI": "A mushroom is a _____ to have at parties",
+	"POWER NAP": "Exercising your authority and sleeping on someone weaker than you",
+	"BROKER": "Person who makes you go broke",
+	"CATHOLIC": "Photons have mass. Therefore they are:",
+	"SOY MILK": "Milk introducing itself in Spanish",
+	"MOSES": "First person to use Ctrl-C",
+	"ODD NUMBERS": "Literally can't even.",
+	"TWITTER": "Voldemort has followers, not friends. Therefore, he uses:",
+		
+	# Shower thoughts
+	"JAPAN": "Has a flag that is a pie chart of how much of itself is itself",
+	"CANADA GEESE": "Proof that dinosaurs would still be scary with feathers",
+	"ANTARCTICA": "Island that only has a northern coast",
+	"BAYONET": "Bringing a knife to a gun fight",
+	"VAMPIRES": "If telescopes work using mirrors, then you'll never know if these exist in space",
+	"STOMACH": "Thinks all potatoes are mashed",
+	"HORSES": "Vehicles with automatic collision avoidance and the capability of self-driving home when you're asleep",
+	
+	# Book synopses
+	"THE ILLIAD": "Achilles spends ten years punching people in the face",
+	
+	# Quotes
+	"GOOGLE STREET VIEW": "\"Yeah, just send someone to drive down every single road on the planet\"",
+	"LUIS SUAREZ": "\"But ref! I only bit someone like 3 times. If you consider all the games I've played, that's almost a perfect record!\"",
+	"LEEEROOOOOOOOY JENKINNNSSSS": "\"Alright chums, I'm back. Let's do this. *inhales*\"",
+	"CELSIUS": "\"On a scale from 0 to 100, how hot is your water?\"",
+	"THE LOS ANGELES ANGELS": "\"The The Angels Angels\""
+	
+	}
